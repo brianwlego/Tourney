418a6a4c3a18b2d337584185ca9a0821f210510b
brianwlego <legobw@gmail.com>
2021-01-31 16:53:44 -0500

    slight change to auth file

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,15 +1,15 @@
 const jsonwebtoken = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const authHeader = req.cookies.token
-  if (!authHeader){
+  const token = req.cookies.token
+  if (!token){
     const err = new Error('Not authenticated')
     err.statusCode = 401;
     throw err;
   }
   let decodedToken;
   try {
-    decodedToken = jsonwebtoken.verify(authHeader, 'brianandryansecret')
+    decodedToken = jsonwebtoken.verify(token, 'brianandryansecret')
     // console.log("decoded token", decodedToken)
   } catch(err) {
     err.statusCode = 500;
